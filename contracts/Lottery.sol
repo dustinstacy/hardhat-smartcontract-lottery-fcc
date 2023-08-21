cbe41de93426d9353ccf6ffc53d1a66ddf65b081
Dustin Stacy <dustin.stacy@outlook.com>
2023-08-21 14:11:09 -0400

    Add fulfillRandomWords function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,19 +5,23 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Lottery__NotEnoughEthEntered(uint256 required, uint256 sent);
+error Lottery__TransferFailed();
 
 contract Lottery is VRFConsumerBaseV2 {
-    uint256 private immutable i_entranceFee;
-    address payable[] private entrants;
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint256 private immutable i_entranceFee;
     bytes32 private immutable i_gasLane;
     uint64 private immutable i_subscriptionId;
     uint32 private immutable i_callbackGasLimit;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint16 private constant NUM_WORDS = 1;
 
+    address payable[] private entrants;
+    address private recentWinner;
+
     event LotteryEntered(address indexed player);
     event RequestedLotteryWinner(uint256 indexed requestId);
+    event WinnerPicked(address indexed winner);
 
     constructor(
         address vrfCoordinatorV2,
@@ -56,9 +60,18 @@ contract Lottery is VRFConsumerBaseV2 {
     }
 
     function fulfillRandomWords(
-        uint256 requestId,
+        uint256 /* requestId */,
         uint256[] memory randomWords
-    ) internal override {}
+    ) internal override {
+        uint256 indexOfWinner = randomWords[0] % entrants.length;
+        address payable mostRecentWinner = entrants[indexOfWinner];
+        recentWinner = mostRecentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Lottery__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
@@ -70,5 +83,9 @@ contract Lottery is VRFConsumerBaseV2 {
 
     function getEntrants() public view returns (address payable[] memory) {
         return entrants;
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return recentWinner;
     }
 }
