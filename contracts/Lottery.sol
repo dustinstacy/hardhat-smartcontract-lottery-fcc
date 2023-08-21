7f7fbd4ade329e7922e15c0392529b23e36dab03
Dustin Stacy <dustin.stacy@outlook.com>
2023-08-21 14:01:41 -0400

    Add VRF Coordinator

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,20 +2,35 @@
 pragma solidity ^0.8.16;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Lottery__NotEnoughEthEntered(uint256 required, uint256 sent);
 
 contract Lottery is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     address payable[] private entrants;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint16 private constant NUM_WORDS = 1;
 
     event LotteryEntered(address indexed player);
+    event RequestedLotteryWinner(uint256 indexed requestId);
 
     constructor(
         address vrfCoordinatorV2,
-        uint256 entranceFee
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
     ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterLottery() public payable {
@@ -29,7 +44,16 @@ contract Lottery is VRFConsumerBaseV2 {
         emit LotteryEntered(msg.sender);
     }
 
-    function requestRandomWinner() external {}
+    function requestRandomWinner() external {
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedLotteryWinner(requestId);
+    }
 
     function fulfillRandomWords(
         uint256 requestId,
