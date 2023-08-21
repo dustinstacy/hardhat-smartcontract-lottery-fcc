91521ec35e30bf6735b89418fbb10217ad942337
Dustin Stacy <dustin.stacy@outlook.com>
2023-08-21 13:16:48 -0400

    Add enterLottery functino and Lottery.test.ts

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,7 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+error Lottery__NotEnoughEthEntered(uint256 required, uint256 sent);
+
 contract Lottery {
-    // function enterRaffle() {}
+    uint256 private immutable i_entranceFee;
+    address payable[] private entrants;
+
+    event LotteryEntered(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterLottery() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Lottery__NotEnoughEthEntered({
+                required: i_entranceFee,
+                sent: msg.value
+            });
+        }
+        entrants.push(payable(msg.sender));
+        emit LotteryEntered(msg.sender);
+    }
+
     // function pickRandomWinner {}
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getEntrant(uint256 index) public view returns (address) {
+        return entrants[index];
+    }
+
+    function getEntrants() public view returns (address payable[] memory) {
+        return entrants;
+    }
 }
---
+++ test/Lottery.test.ts
@@ -0,0 +1,9 @@
+describe('Lottery', () => {
+    beforeEach(() => {})
+
+    describe('enterLottery', () => {
+        it('Reverts the transaction if there is not enough ETH', () => {})
+        it('Adds entrant to entrants array', () => {})
+        it('Emits a new Lottery Entered event for each new entrant', () => {})
+    })
+})
