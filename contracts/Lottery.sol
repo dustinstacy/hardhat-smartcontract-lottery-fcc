ba6862facd3e655b15c0edee55e928885ef87e0e
Dustin Stacy <dustin.stacy@outlook.com>
2023-08-21 13:44:10 -0400

    Add VRFConsumerBaseV2

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Lottery__NotEnoughEthEntered(uint256 required, uint256 sent);
 
-contract Lottery {
+contract Lottery is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     address payable[] private entrants;
 
     event LotteryEntered(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
     }
 
@@ -24,7 +29,12 @@ contract Lottery {
         emit LotteryEntered(msg.sender);
     }
 
-    // function pickRandomWinner {}
+    function requestRandomWinner() external {}
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
