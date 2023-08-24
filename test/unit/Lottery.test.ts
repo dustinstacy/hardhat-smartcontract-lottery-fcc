56830058cb4c5b2cee9ecedcf325d61daf66337e
Dustin Stacy <dustin.stacy@outlook.com>
2023-08-24 11:30:31 -0400

    Add fullfillRandomWords tsts

--- test/unit/Lottery.test.ts
+++ test/unit/Lottery.test.ts
@@ -50,8 +50,8 @@ import { expect } from 'chai'
 
               it('initalizes the subscription id correctly', async () => {
                   let subscriptionId = await lottery.getSubscriptionId()
-                  const transaction = await vrfCoordinatorV2Mock.createSubscription()
-                  const receipt = await transaction.wait(1)
+                  const tx = await vrfCoordinatorV2Mock.createSubscription()
+                  const receipt = await tx.wait(1)
                   subscriptionId++ // Calling createSubscription() again increments the indexed _subId so we match that here
                   expect(subscriptionId).to.equal(receipt!.logs[0].topics[1])
               })
@@ -86,6 +86,7 @@ import { expect } from 'chai'
                       'Lottery__NotEnoughEthSent'
                   )
               })
+
               it('Denies entry when lottery is in a Closed state', async () => {
                   await lottery.enterLottery({ value: entranceFee })
                   await network.provider.send('evm_mine', [])
@@ -95,11 +96,13 @@ import { expect } from 'chai'
                       lottery.enterLottery({ value: entranceFee })
                   ).to.be.revertedWithCustomError(lottery, 'Lottery__Closed')
               })
+
               it('Adds entrant to entrants array upon entry!', async () => {
                   await lottery.enterLottery({ value: entranceFee })
                   const entrantFromContract = await lottery.getEntrant(0)
                   expect(entrantFromContract).to.equal(deployer.address)
               })
+
               it('Emits a new Lottery Entered event for each new entrant', async () => {
                   await expect(lottery.enterLottery({ value: entranceFee })).to.emit(
                       lottery,
@@ -119,6 +122,7 @@ import { expect } from 'chai'
                   expect(lotteryState).to.equal(1)
                   expect(upkeepNeeded).to.be.false
               })
+
               it('Returns false if not enough time has passed', async () => {
                   await lottery.enterLottery({ value: entranceFee })
                   await network.provider.send('evm_increaseTime', [interval - 5])
@@ -126,12 +130,14 @@ import { expect } from 'chai'
                   const { upkeepNeeded } = await lottery.checkUpkeep.staticCall('0x')
                   expect(upkeepNeeded).to.be.false
               })
+
               it('Return false if there are no entrants', async () => {
                   await network.provider.send('evm_increaseTime', [interval + 1])
                   await network.provider.send('evm_mine', [])
                   const { upkeepNeeded } = await lottery.checkUpkeep.staticCall('0x')
                   expect(upkeepNeeded).to.be.false
               })
+
               it('Returns true if is open, enough time has passed, and has entrants', async () => {
                   await lottery.enterLottery({ value: entranceFee })
                   await network.provider.send('evm_increaseTime', [interval + 1])
@@ -141,5 +147,49 @@ import { expect } from 'chai'
               })
           })
 
-          describe('performUpKeep', async () => {})
+          describe('performUpKeep', async () => {
+              beforeEach(async () => {
+                  await network.provider.send('evm_increaseTime', [interval + 1])
+                  await network.provider.send('evm_mine', [])
+              })
+
+              it('Should revert if upkeep is not needed', async () => {
+                  await expect(lottery.performUpkeep('0x')).to.be.revertedWithCustomError(
+                      lottery,
+                      'Lottery__UpkeepNotNeeded'
+                  )
+              })
+
+              it('Should set the lottery state to Closed', async () => {
+                  await lottery.enterLottery({ value: entranceFee })
+                  await lottery.performUpkeep('0x')
+                  const lotteryState = await lottery.getLotteryState()
+                  expect(lotteryState).to.equal(1)
+              })
+
+              it('Should call the requestRandomWords function and stores it as a requestId', async () => {
+                  await lottery.enterLottery({ value: entranceFee })
+                  const tx = await lottery.performUpkeep('0x')
+                  const txReceipt = await tx.wait(1)
+                  const requestId = txReceipt!.logs[1].topics[1]
+                  expect(requestId).to.be.greaterThan(BigInt(0))
+              })
+
+              it('Should emit a Requested Random Lottery winner event', async () => {
+                  await lottery.enterLottery({ value: entranceFee })
+                  await expect(lottery.performUpkeep('0x')).to.emit(
+                      lottery,
+                      'RequestedLotteryWinner'
+                  )
+              })
+          })
+
+          describe('fulfillRandomWords', () => {
+              beforeEach(async () => {
+                  await lottery.enterLottery({ value: entranceFee })
+                  await network.provider.send('evm_increaseTime', [interval + 1])
+                  await network.provider.send('evm_mine', [])
+              })
+              it('Is only called after performUpkeep', async () => {})
+          })
       })
